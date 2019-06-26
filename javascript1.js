916192190b2cc1354e52257b3fd66991e688eae0
Lovedeep Singh <748774@student.tor.loc>
2019-06-26 14:17:23 -0400

    2.15 COMMMIT

--- javascript1.js
+++ javascript1.js
@@ -1,3 +1,11 @@
+function waitingFn(timeInMs){
+    const futureTime =Date.now()+timeInMs;
+
+    while(futureTime>Date.now()){
+        //waiting 
+    }
+}
+
 waitingFn(3000);
 
 console.log("Function call has just ended");
